fd9d6529580b3c315b12108ffd2d1322675760d0
Daniel Merritt <daniel.mafou@gmail.com>
2023-06-01 17:00:10 -0400

    stub up home html with css connection

--- main_app/views.py
+++ main_app/views.py
@@ -1,9 +1,8 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 
 # CBVs
 
 # View Functions
 def home(request):
-  return HttpResponse('<h1>Home Page</h1>')
+  return render(request, 'home.html')
