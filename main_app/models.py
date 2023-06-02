fa4f79049c31bd760f4122f2040e2b82d775483a
Daniel Merritt <daniel.mafou@gmail.com>
2023-06-02 14:19:30 -0400

    implement auth

--- main_app/models.py
+++ main_app/models.py
@@ -1,13 +1,26 @@
 from django.db import models
 from django.urls import reverse
+from django.contrib.auth.models import User
+
+LANGUAGES = (
+  ('H', 'HTML5'),
+  ('C', 'CSS'),
+  ('J', 'JavaScript'),
+  ('P', 'Python'),
+)
 
 # Create your models here.
 class Code(models.Model):
   name = models.CharField(max_length=100)
   description = models.TextField(max_length=300)
+  language = models.CharField(
+    max_length=1,
+    choices=LANGUAGES,
+    default=LANGUAGES[0][0])
   code = models.TextField(max_length=500)
   github = models.CharField(max_length=100)
   star = models.BooleanField()
+  user = models.ForeignKey(User, on_delete=models.CASCADE)
 
   def __str__(self):
     return self.name
--- main_app/views.py
+++ main_app/views.py
@@ -1,30 +1,59 @@
-from django.shortcuts import render
+from django.contrib.auth import login
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.views import LoginView
+
+from django.shortcuts import render, redirect
+
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
+
 from .models import Code
 
 
 # CBVs
-class CodeCreate(CreateView):
+class Home(LoginView):
+  template_name = 'home.html'
+
+class CodeCreate(LoginRequiredMixin, CreateView):
   model = Code 
-  fields = '__all__'
+  fields = ['name', 'description', 'code', 'github', 'language', 'star']
   success_url = '/codes/'
 
-class CodeUpdate(UpdateView):
-  model = Code
-  fields = ['description', 'code', 'github', 'star']
+  def form_valid(self, form):
+    form.instance.user = self.request.user 
+    return super().form_valid(form)
 
-class CodeDelete(DeleteView):
+class CodeUpdate(LoginRequiredMixin, UpdateView):
+  model = Code
+  fields = ['description', 'code', 'github', 'language', 'star']
+
+class CodeDelete(LoginRequiredMixin, DeleteView):
   model = Code
   success_url = '/codes/'
 
 # View Functions
-def home(request):
-  return render(request, 'home.html')
 
+@login_required
 def code_index(request):
-  codes = Code.objects.all()
+  codes = Code.objects.filter(user=request.user)
   return render(request, 'codes/index.html', { 'codes' : codes})
 
+@login_required
 def code_detail(request, code_id):
   code = Code.objects.get(id=code_id)
   return render(request, 'codes/detail.html', { 'code': code })
+
+def signup(request):
+  error_message = ''
+  if request.method == 'POST':
+    form = UserCreationForm(request.POST)
+    if form.is_valid():
+      user = form.save()
+      login(request, user)
+      return redirect('cat-index')
+    else:
+      error_message = 'Invalid sign up - try again'
+  form = UserCreationForm()
+  context = {'form': form, 'error_message': error_message}
+  return render(request, 'signup.html', context)
